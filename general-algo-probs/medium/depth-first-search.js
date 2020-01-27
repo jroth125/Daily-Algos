d915b4f1a67819c086b264a9af9ab3298b116af6
Jake Rothenberg <jrothenberg125@gmail.com>
2020-01-27 10:17:39 -0500

    breadth-first added

--- general-algo-probs/medium/depth-first-search.js
+++ general-algo-probs/medium/depth-first-search.js
@@ -1,25 +1,25 @@
-// Feel free to add new properties
-// and methods to the class.
+
 class Node {
-  constructor(name) {
-    this.name = name;
-    this.children = [];
-  }
-
-  addChild(name) {
-    this.children.push(new Node(name));
-    return this;
-  }
-
-  breadthFirstSearch(array = []) {
-    // Write your code here.
-    let currNode = this;
-    let queue = [this];
-    while (queue.length) {
-      queue.push(...currNode.children);
-      currNode = queue.shift
-      array.push(currNode.value)
+    constructor(name) {
+      this.name = name;
+      this.children = [];
     }
-    return array
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    breadthFirstSearch(array = []) {
+      // Write your code here.
+      let currNode;
+      let queue = [this];
+      while (queue.length) {
+              currNode = queue.shift()
+              array.push(currNode.name)
+              queue.push(...currNode.children)
+        
+      }
+      return array
+    }
   }
-}
