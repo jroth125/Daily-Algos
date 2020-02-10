43885f7d81171cfd42deef1e62fced1c9ed2f91e
Jake Rothenberg <jrothenberg125@gmail.com>
2020-02-10 15:39:06 -0500

    youngest common ancestor done with a map

--- general-algo-probs/medium/youngest-common-ancester.js
+++ general-algo-probs/medium/youngest-common-ancester.js
@@ -1,3 +1,17 @@
-function youngestCommonAncestor() {
-    
+function youngestCommonAncestor(topAncestor, descendentOne, descendentTwo) {
+    const ancestorsOne = new Map
+    let currAncestor = descendentOne
+
+    while (currAncestor !== topAncestor) {
+        ancestorsOne.set(currAncestor, true)
+        currAncestor = currAncestor.ancestor
+    }
+
+    let currAncestor2 = descendentTwo
+    while (currAncestor2 !== topAncestor) {
+        if (ancestorsOne.get(currAncestor2)) return currAncestor2
+        currAncestor2 = currAncestor2.ancestor
+    }
+
+    return topAncestor
 }
