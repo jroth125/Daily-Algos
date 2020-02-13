58d1b8bb2a0f9f8115862a8b498bb9677c4b673d
Jake Rothenberg <jrothenberg125@gmail.com>
2020-02-13 12:11:19 -0500

    longest Palindromic substring explanation

--- general-algo-probs/medium/longest-palindromic-substring.js
+++ general-algo-probs/medium/longest-palindromic-substring.js
@@ -1,10 +1,17 @@
 
 /*
 Write a function that, given a string, returns its longest palindromic 
-substring. A palindrome is defined as a string that is written the same 
-forward and backward. Assume that there will only be one longest 
-palindromic substring.
+substring.
+
+Example input: abaxyzzyxf
+Example output: xyzzyx
 */ 
+
+// This is my O(n^2) solution. It iterates through each letter in the string, and then
+// does two different palindrome checks, each with a different middle.
+// The first has a middle of the letter itself. 
+// The second check has a middle of between the current letter and the next letter.
+
 
 function longestPalindromicSubstring(string) {
     if (string.length === 1 || !string.length) return string;
@@ -12,6 +19,8 @@ function longestPalindromicSubstring(string) {
     let palindrome = '';
   
     for (let i = 0; i < string.length; i++) {
+
+    // First while loop, with current letter as the middle
       let left = i - 1;
       let right = i + 1;
       let temp = 1;
@@ -29,11 +38,12 @@ function longestPalindromicSubstring(string) {
         count = temp;
       }
   
+    //   Second while loop scenario, with between i and i+1 as the middle
       left = i;
       right = i + 1;
       temp = 0;
       curPal = '';
-  
+
       while (left >= 0 && right < string.length) {
         if (string[left] === string[right]) {
           curPal = string[left] + curPal + string[right];
