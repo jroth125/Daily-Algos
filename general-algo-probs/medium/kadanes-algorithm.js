7c0c9465fbb94c25ac0ead01e2a90cb6996b255d
Jake Rothenberg <jrothenberg125@gmail.com>
2020-02-24 14:37:01 -0500

    optimal solution

--- general-algo-probs/medium/kadanes-algorithm.js
+++ general-algo-probs/medium/kadanes-algorithm.js
@@ -1,21 +1,29 @@
-// non-optimal solution
-function kadanesAlgorithm(array) {
+// // non-optimal solution
+// function kadanesAlgorithm(array) {
 
-    let most = -Infinity
-    for (let i = 0; i < array.length; i++) {
-        let curNum = array[i]
+//     let most = -Infinity
+//     for (let i = 0; i < array.length; i++) {
+//         let curNum = array[i]
 
-        for (let j = 0; j < i; j++) {
-            let curSum = curNum
-            for (let k = j; k < i; k++) {
-                curSum += array[k]
-            }
-            most = curSum > most ? curSum : most
-            curSum = -Infinity
-        }
-        most = curNum > most ? curNum : most
-    }
-    return most
+//         for (let j = 0; j < i; j++) {
+//             let curSum = curNum
+//             for (let k = j; k < i; k++) {
+//                 curSum += array[k]
+//             }
+//             most = curSum > most ? curSum : most
+//             curSum = -Infinity
+//         }
+//         most = curNum > most ? curNum : most
+//     }
+//     return most
+//   }
+
+  function kadanesAlgorithm (array) {
+      let sums = array.map(num => num)
+      for (let i = 1; i < array.length; i++) {
+          sums[i] = sums[i] + sums[i - 1] > sums[i] ? sums[i] + sums[i - 1] : sums[i]
+      }
+      return Math.max(...sums)
   }
   
 
