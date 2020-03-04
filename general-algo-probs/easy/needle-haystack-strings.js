58ac477d513246da258ecbbcb3e6e0d0c03733e0
Jake Rothenberg <jrothenberg125@gmail.com>
2020-03-03 23:16:28 -0500

    haystack solutions

--- general-algo-probs/easy/needle-haystack-strings.js
+++ general-algo-probs/easy/needle-haystack-strings.js
@@ -1,5 +1,5 @@
 const needleInHaystack = (haystack, needle) => {
-    for (let i = 0; i < haystack.length; i++) {
+    for (let i = 0; i < haystack.length - needle.length; i++) {
         for (let j = 0; j < needle.length; j++) {
             if (haystack[j + i] !== needle[j]) break;
             if ((j + 1) === needle.length) return i
@@ -7,3 +7,20 @@ const needleInHaystack = (haystack, needle) => {
     }
     return -1
 }
+
+console.log(needleInHaystack('hellow oreeeasdfadfworld', 'oreee'))
+
+const betterHaystack = (haystack, needle) => {
+    let left = 0
+    let right = needle.length - 1
+    let curWord = haystack.slice(0, needle.length)
+
+    while (right < haystack.length) {
+        if (curWord === needle) return left
+        ++right; ++left;
+        curWord = curWord.slice(1) + haystack[right]
+    }
+    return -1
+}
+
+console.log('better haystack', betterHaystack('a better world', 'or'))
