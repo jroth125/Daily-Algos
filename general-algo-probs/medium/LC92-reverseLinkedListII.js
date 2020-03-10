ba81eebc0f75cbdf3c9d0e07421faf6a9bd6e7e8
Jake Rothenberg <jrothenberg125@gmail.com>
2020-03-10 13:59:04 -0400

    added: reverseLinkedListII first take

---
+++ general-algo-probs/medium/LC92-reverseLinkedListII.js
@@ -0,0 +1,34 @@
+const reverseLinkedList = (head, m, n) => {
+    let curNode = head, counter = 0
+    let chunkOneEnd, chunkTwoStart, chunkTwoEnd, chunkThreeStart;
+    while (curNode) {
+        ++counter
+        if (counter === m - 1) chunkOneEnd = curNode
+        if (counter === m) chunkTwoStart = curNode
+        if (counter === n) chunkTwoEnd = curNode
+        if (counter === n + 1) chunkThreeStart = curNode
+        curNode = curNode.next 
+    }
+
+    chunkOneEnd.next = null
+    curNode = chunkTwoStart.next
+    let lastNode = chunkTwoStart
+    let newCount = 0
+    while (newCount <= (n - m) + 1) {
+        ++newCount
+        console.log(curNode)
+        let newNode = curNode.next
+        curNode.next = lastNode
+        lastNode = curNode
+        curNode = newNode
+    }
+    chunkTwoStart.next = chunkThreeStart
+    chunkOneEnd.next = chunkTwoEnd
+
+    return head
+}
+
+function reverseList(curNode, lastNode) {
+    lastNode.next = null
+
+}
