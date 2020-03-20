7043c77e8fc94d18897386eaf604f004b4f4c661
Jake Rothenberg <jrothenberg125@gmail.com>
2020-03-20 16:22:24 -0400

    edit: groupAnagrams more optimal solution

--- general-algo-probs/medium/group-anagrams.js
+++ general-algo-probs/medium/group-anagrams.js
@@ -3,14 +3,12 @@ function groupAnagrams(words) {
     let prevWords = {}
     for (let i = 0; i < words.length; i++) {
         let curWord = sortedWords[i]
-        if (!prevWords[curWord]) prevWords[curWord] = [i]
-        else prevWords[curWord].push(i)
+        if (!prevWords[curWord]) prevWords[curWord] = [words[i]]
+        else prevWords[curWord].push(words[i])
     }
-    return Object.keys(prevWords).map(word => {
-        return prevWords[word].map(idx => {
-            return words[idx]
-        })
-    })
+    return Object.values(prevWords)
 }
+
+
 
 console.log(groupAnagrams(["yo", "act", "flop", "tac", "cat", "oy", "olfp"]))
--- general-algo-probs/medium/invert-binary-tree.js
+++
@@ -1,8 +0,0 @@
-function invertBinaryTree(tree) {
-    if (!tree.right && !tree.left) return;
-    let left = tree.left
-    tree.left = tree.right
-    tree.right = left
-    if (tree.right) invertBinaryTree(tree.right)
-    if (tree.left) invertBinaryTree(tree.left)
-}
