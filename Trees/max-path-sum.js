7962a986e21b45fab5e066918fdba2a752005d19
Jake Rothenberg <jrothenberg125@gmail.com>
2020-04-13 16:30:45 -0400

    edit: added instructions to max path sum

--- Trees/max-path-sum.js
+++ Trees/max-path-sum.js
@@ -1,3 +1,21 @@
+/*
+AlgoExpert: MaxPathSum in a Binary Tree
+
+    A path is a collection of connected nodes in a tree where no node is connected
+  to more than two other nodes; a path sum is the sum of the values of the nodes
+  in a particular path.
+
+  get the max path sum in a binary tree. 
+
+       1
+    /     \
+   2       3
+ /   \   /   \
+4     5 6     7
+
+here max path sum is 18 because of 5 + 2 + 1 + 3 + 7
+*/
+
 function maxPathSum(tree) {
   let [maxBranchRight, runMaxRight] = tree.right ? maxSums(tree.right) : [0, 0];
   let [maxBranchLeft, runMaxLeft] = tree.left ? maxSums(tree.left) : [0, 0];
