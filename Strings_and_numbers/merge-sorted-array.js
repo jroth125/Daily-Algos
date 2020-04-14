dd70aa2ec446da683d78b9056ad2df40c8891f5a
Jake Rothenberg <jrothenberg125@gmail.com>
2020-04-14 18:47:33 -0400

    edit: added time and space complexity in merge-sorted-array.js

--- Strings_and_numbers/merge-sorted-array.js
+++ Strings_and_numbers/merge-sorted-array.js
@@ -17,7 +17,8 @@ nums2 = [2,5,6],       n = 3
 Output: [1,2,2,3,5,6]
 */
 
-
+// O(n + m) time and O(n + m) space 
+// Here we push into our own array and then copy the numbers in that array to nums1
 const mergeSortedArray = (nums1, m, nums2, n) => {
     let merged = []
     let p1 = 0
@@ -49,3 +50,34 @@ function pushRest(merged, pointer, numsArr, end) {
         merged.push(numsArr[i])
     }
 }
+
+
+
+
+
+// O(n + m) time and O(1) space (in place)
+// Here we have pointers p1 and p2 which are tracking the current numbers to compare
+// and we also have p, which is tracking the next index in the nums1 array to swap
+const mergeBetter = (nums1, m, nums2, n) => {
+    debugger;
+    let p = nums1.length - 1
+    let p1 = m - 1
+    let p2 = n - 1
+    
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums2[p2] > nums1[p1]) {
+            nums1[p] = nums2[p2]
+            --p2; --p
+        } else {
+            nums1[p] = nums1[p1]
+            --p1; --p
+        }
+    }
+    while (p2 >= 0){
+        nums1[p] = nums2[p2]
+        --p2; --p;
+    }
+    return nums1
+}
+
+console.log(mergeBetter([1,2,3,0,0,0], 3, [2,5,6], 3))
