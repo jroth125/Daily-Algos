9d559914f6512a39fc175770986fa917489934d5
Jake Rothenberg <jrothenberg125@gmail.com>
2020-05-03 16:26:05 -0400

    add: cleaner remove to make parentheses valid add

--- Strings_and_numbers/minimum-remove-parentheses-valid.js
+++ Strings_and_numbers/minimum-remove-parentheses-valid.js
@@ -1,4 +1,28 @@
-const minRemoveToMakeValid = function(s) {
+function cleanerRemoveToMakevalid(string) {
+    let leftCount = rightCount = 0
+    let keepChar = Array(string.length).fill(true)
+    let lIdx = 0
+    let rIdx = string.length - 1
+    while(lIdx < string.length) {
+        let rightChar = string[rIdx]
+        let leftChar = string[lIdx]
+        if (rightChar === '(') {
+            if (rightCount === 0) keepChar[rIdx] = false
+            else rightCount--
+        } else if (rightChar === ')') rightCount++
+        if (leftChar === ')') {
+            if (leftCount === 0) keepChar[lIdx] = false
+            else leftCount--
+        } else if (leftChar === '(') leftCount++
+        lIdx++; rIdx--
+    }
+    return keepChar.reduce((charSum, char, i) => {
+        if (!char) return charSum + ''
+        else return charSum + string[i]
+    }, '')
+}
+
+const minRemoveToMakeValid = (s) => {
     let newString = Array(s.length).fill(0)
     let count = 0
     for (let i = 0; i < s.length; i++){
